b05ba7311c54f71aabc87ae8c27d8fb7cfedc1e0
Fitsum Gedefaw <gedefawfitsum@gmail.com>
2024-10-30 12:02:03 +0300

    Revert "Update the footer"
    This reverts commit b2d60c78cf9941f2f8d18db820dc5254bfe523d0.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
 	const { pathname } = useRouter();
 
 	return (
-		<div className="sticky -bottom-32 left-0 z-20 mt-3 w-full bg-black py-3 sm:fixed sm:bottom-0 lg:relative lg:px-5">
+		<div className=" my-scroll sticky -bottom-32 left-0 z-20 mt-3 w-full bg-black py-3 sm:fixed sm:bottom-0 lg:relative lg:px-5">
 			<div className="mb-20 flex flex-col justify-center gap-3 sm:mb-0 sm:flex-row ">
 				<ul className="mx-auto hidden flex-1 gap-1 lg:flex lg:max-w-full">
 					{sideLinks.map((data) => {
@@ -37,7 +37,7 @@ function Footer() {
 					</Link>
 				</div>
 				<p className="flex-1 text-center text-sm text-white sm:flex-[2]">
-					Created by <span className="text-[#1e9c52]">Fitsum Gedefaw</span>{" "}
+					Created by <span className="text-[#1e9c52]">Kamasah Dickson</span>{" "}
 					with 💚
 				</p>
 				<p className="flex-1 text-center text-sm text-white sm:flex-[2]">
